7090298455958cee510cd55157725a2faead6261
Yuna Morgenstern <io@yuna.berlin>
2021-11-14 11:31:10 +0100

    chore: updated readme

--- src/main/java/berlin/yuna/repos/Main.java
+++ src/main/java/berlin/yuna/repos/Main.java
@@ -5,6 +5,7 @@ import javax.json.JsonValue;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
@@ -21,6 +22,8 @@ import static java.util.stream.Collectors.joining;
 
 public class Main {
 
+    private static final String LINE_SEPARATOR = lineSeparator();
+
     //TODO:
     // mvn wrapper
     // mvn dependency update
@@ -28,8 +31,6 @@ public class Main {
     // gitignore
     // git workflow template
     public static void main(String[] args) throws IOException {
-        final var result = new StringBuilder();
-        result.append("[![YunaBraska's GitHub stats](https://github-readme-stats.vercel.app/api?username=YunaBraska&count_private=true&show_icons=true&theme=dracula)](https://github.com/YunaBraska/github-readme-stats)").append(lineSeparator()).append(lineSeparator());
         REPO_MAP.clear();
         final var repos = getArray("https://api.github.com/users/YunaBraska/repos");
         for (JsonValue repoRaw : repos) {
@@ -38,7 +39,8 @@ public class Main {
             final var name = repo.getString("name");
             final var html_url = repo.getString("html_url");
             final var repoId = repo.getString("full_name");
-            if (!tags.isEmpty()) {
+            if (!tags.isEmpty() && !repo.getBoolean("archived") && !repo.getBoolean("disabled")) {
+                System.out.println("Updating [" + name + "]");
                 addRow(name, "Name", name, html_url);
                 addRow(name, "Tag", tags.iterator().next().asJsonObject().getString("name"), html_url + "/tags");
                 addRow(name, "Updated", ZonedDateTime.parse(repo.getString("updated_at"), DateTimeFormatter.ISO_ZONED_DATE_TIME).format(DateTimeFormatter.ISO_LOCAL_DATE));
@@ -48,13 +50,34 @@ public class Main {
                 addRow(name, "Maintainability", "![maintainability](https://img.shields.io/codeclimate/maintainability/" + repoId + "?style=flat-square)");
                 addRow(name, "Coverage", "![coverage](https://img.shields.io/codeclimate/coverage/" + repoId + "?style=flat-square)");
                 addRow(name, "Description", repo.getString("description"));
+            } else {
+                System.out.println("Skipped [" + name + "]");
             }
         }
-        LinkedHashMap<String, LinkedHashMap<String, String>> sortedMap = new LinkedHashMap<>();
+        writeReadmeFile();
+    }
+
+    private static void writeReadmeFile() throws IOException {
+        final var result = new StringBuilder();
+        final var sortedMap = new LinkedHashMap<String, LinkedHashMap<String, String>>();
         REPO_MAP.entrySet().stream().sorted(Map.Entry.comparingByValue((o1, o2) -> String.CASE_INSENSITIVE_ORDER.compare(o2.get("Updated"), o1.get("Updated")))).forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
-        result.append(sortedMap.values().iterator().next().keySet().stream().collect(joining("|", "|", "|"))).append(lineSeparator());
-        result.append(sortedMap.values().iterator().next().keySet().stream().map(s -> "---").collect(joining("|", "|", "|"))).append(lineSeparator());
-        sortedMap.forEach((row, column) -> result.append(column.values().stream().collect(joining("|", "|", "|"))).append(lineSeparator()));
+        result
+                .append("[![YunaBraska's GitHub stats](https://github-readme-stats.vercel.app/api?username=YunaBraska&count_private=true&show_icons=true&theme=dracula)](https://github.com/YunaBraska/github-readme-stats)")
+                .append(LINE_SEPARATOR)
+                .append(LINE_SEPARATOR)
+                //TABLE HEAD START
+                .append(sortedMap.values().iterator().next().keySet().stream().collect(joining("|", "|", "|")))
+                .append(LINE_SEPARATOR)
+                //TABLE HEAD END
+                .append(sortedMap.values().iterator().next().keySet().stream().map(s -> "---").collect(joining("|", "|", "|")))
+                .append(LINE_SEPARATOR);
+        //TABLE CONTENT START
+        sortedMap.forEach((row, column) -> result.append(column.values().stream().collect(joining("|", "|", "|" + LINE_SEPARATOR))));
+        //TABLE CONTENT END
+        result
+                .append(LINE_SEPARATOR)
+                .append("Last update [").append(ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_LOCAL_DATE)).append("]")
+                .append(LINE_SEPARATOR);
         Files.write(Path.of(PROJECT_DIR, "README.md"), result.toString().getBytes(UTF_8));
     }
 
